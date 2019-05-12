42a9762254ff791e95efa0ec16ee777b969166b0
Dmytro Kholodov <surakamy@gmail.com>
2019-05-12 22:37:20 +0300

    Swift on Sundays - JustType - initial commit
    Building document-based app.

---
+++ JustType/JustType/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  JustType
+//
+//  Created by Dmytro Kholodov on 5/12/19.
+//  Copyright © 2019 HackingWithSwift. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
