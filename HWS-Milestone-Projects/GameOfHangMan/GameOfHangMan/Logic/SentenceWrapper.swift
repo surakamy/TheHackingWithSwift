b8aa75ba82297375feb524e38105becca1938e24
Dmytro Kholodov <surakamy@gmail.com>
2019-05-18 15:51:36 +0300

    Day 46

---
+++ HWS-Milestone-Projects/GameOfHangMan/GameOfHangMan/Logic/SentenceWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  SentenceWrapper.swift
+//  GameOfHangMan
+//
+//  Created by Dmytro Kholodov on 5/17/19.
+//  Copyright © 2019 HackingWithSwift. All rights reserved.
+//
+
+import Foundation
